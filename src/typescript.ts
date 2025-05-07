65753df3dad0e7ed9b5fe51b82d599b34aac53b6
Orinakter <orin.akter9013@gmail.com>
2025-05-07 23:20:30 +0600

    solve problem number-2

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,7 +1,17 @@
 {
-    function formatString(input: string, toUpper: boolean = true): string {
-        const result = toUpper? input.toUpperCase() : input.toLowerCase()
-        return result;
-    }
-     
+  function formatString(input: string, toUpper: boolean = true): string {
+    const result = toUpper ? input.toUpperCase() : input.toLowerCase();
+    return result;
+  }
+
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    return items.filter((item) => item.rating >= 4);
+  }
+
+  
+
+
+
 }
