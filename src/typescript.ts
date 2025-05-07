ffddc00c2ad9989bdd9039d3e758aa5f608eedfc
Orinakter <orin.akter9013@gmail.com>
2025-05-07 23:12:37 +0600

    solve problem number-1

---
+++ src/typescript.ts
@@ -0,0 +1,7 @@
+{
+    function formatString(input: string, toUpper: boolean = true): string {
+        const result = toUpper? input.toUpperCase() : input.toLowerCase()
+        return result;
+    }
+     
+}
